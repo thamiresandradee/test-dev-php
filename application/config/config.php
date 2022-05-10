351308e1734b30f1c069073d57bc3a3855d1a538
thamiresandradee <73314800+thamiresandradee@users.noreply.github.com>
2022-05-10 01:59:00 -0300

    Update config.php
    Atualização nome projeto

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ date_default_timezone_set('America/Sao_paulo');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/teste_arizona/';
+$config['base_url'] = 'http://localhost/test_project/';
 
 /*
 |--------------------------------------------------------------------------
